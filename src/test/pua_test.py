9535fde8da0511b24d58a0a754ba352bad59f3a8
Steffen <steffen.hoerhold@web.de>
2021-03-10 13:19:06 +0100

    Delete src/test directory

--- src/test/pua_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-print (chr(0x01) + chr(0x1b) + chr(0x4f), end='')
-for codepoint in range(0xE000, 0xE400):
-    print(chr(codepoint), end='')
-
-    """if (codepoint - 0xE000) % 40 == 39:
-        print()"""

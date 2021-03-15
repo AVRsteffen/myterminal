0f6e4728cb41d6cae2fe21fb38a440198703fe37
Steffen <steffen.hoerhold@web.de>
2021-03-16 00:34:08 +0100

    Add files via upload

---
+++ src/video_controller/registers.v
@@ -0,0 +1,13 @@
+localparam
+    VIDEO_NOP              = 'd0,
+    VIDEO_SET_BASE_ADDRESS = 'd1,
+    VIDEO_SET_FIRST_ROW    = 'd2,
+    VIDEO_CURSOR_POSITION  = 'd3,
+
+    COLUMNS                = 'd40,
+    COLUMNS_REAL           = 'd128,
+    ROWS                   = 'd24,
+    CHARATTR_SIZE          = 'd4,
+    ROW_SIZE               = COLUMNS_REAL * CHARATTR_SIZE,
+    PAGE_SIZE              = ROW_SIZE * ROWS
+    ;
